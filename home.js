0023cee01d121df899efd820586ac439cde2f3d4
Abdullah-bin-azad <abdullahphck1@gmail.com>
2024-12-17 19:27:59 +0600

    new balance added with innerText

--- home.js
+++ home.js
@@ -9,5 +9,25 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     console.log(addMoneyInput);
     // get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput)
+    console.log(pinNumberInput);
+    // step-3 verify pin number
+    // wrong way to validate pin number
+    if( pinNumberInput === '1234'){
+        console.log('adding money to your account')
+         //step-4 get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance)
+       //step-5 add addMoneyInput with balance
+       const addMoneyNumber = parseFloat(addMoneyInput);
+       const balanceNumber = parseFloat(balance);
+       const newBalance = addMoneyNumber + balanceNumber;
+       console.log(addMoneyNumber);
+       console.log(balanceNumber);
+       console.log(newBalance);
+       //step-6 update the balance in UI/DOM
+       document.getElementById('account-balance').innerText = newBalance;
+
+    }else{
+        alert('Failed to add money! Please try again')
+    }
 });
