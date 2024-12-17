2febe85e7673534cab53703201b1a5824b9e7c43
Abdullah-bin-azad <abdullahphck1@gmail.com>
2024-12-17 07:30:36 +0600

    second commit

--- payoo.js
+++ payoo.js
@@ -1,10 +1,31 @@
-//step--01 set event handler
-document.getElementById('login-btn').addEventListener('click',function(event){
-    // step---02 prevent default behavior (prevent reloading browser)
-    event.preventDefault();//reload off
-    console.log('jo ;o');
-    // step---03 get the phone number
-    const phoneNumber = document.getElementById('phone-number').value;
-   console.log(phoneNumber)
+// //step--01 set event handler
+// document.getElementById('login-btn').addEventListener('click',function(event){
+//     // step---02 prevent default behavior (prevent reloading browser)
+//     event.preventDefault();//reload off
+   
+//     // step---03 get the phone number
+//     const phoneNumber = document.getElementById('phone-number').value;
+//    const pinNumber =document.getElementById('pin-number').value;
+//    console.log(phoneNumber,pinNumber);
 
+   
+// if(phoneNumber=='5' && pinNumber=='1234'){
+//     console.log('You are logged in')
+// }else(
+//     alert('Your Mobile or Pin Wrong')
+// )
+
+// })
+document.getElementById('login-btn').addEventListener('click',function(event){
+    console.log('Your button clicked');
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber)
+    if(phoneNumber=='5' && pinNumber=='1234'){
+        console.log('Your are Right login');
+        window.location.href ="/home.html"
+    }else{
+        alert('Your phone or Pin Wrong')
+    }
 })
